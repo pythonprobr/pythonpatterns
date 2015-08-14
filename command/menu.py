aada1ae773b0337f30ab0321831dbca309a2a15e
Luciano Ramalho <luciano@ramalho.org>
2015-08-14 16:54:55 -0300

    adaptando examplo do HFDP

---
+++ command/menu.py
@@ -0,0 +1,39 @@
+
+import abc
+
+class ItemCardapio(abc.ABC):
+
+    @abc.abstractmethod
+    def incluir(self, item):
+        '''incluir
+
+
+public abstract class MenuComponent {
+
+    public void add(MenuComponent menuComponent) {
+        throw new UnsupportedOperationException();
+    }
+    public void remove(MenuComponent menuComponent) {
+        throw new UnsupportedOperationException();
+    }
+    public MenuComponent getChild(int i) {
+        throw new UnsupportedOperationException();
+    }
+
+    public String getName() {
+        throw new UnsupportedOperationException();
+    }
+    public String getDescription() {
+        throw new UnsupportedOperationException();
+    }
+    public double getPrice() {
+        throw new UnsupportedOperationException();
+    }
+    public boolean isVegetarian() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void print() {
+        throw new UnsupportedOperationException();
+    }
+}
