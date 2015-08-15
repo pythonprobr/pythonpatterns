51476cb293b1760a8b164b377e2e092913e96724
Luciano Ramalho <luciano@ramalho.org>
2015-08-15 12:34:38 -0300

    doctest para EmbrulhoRestritor

--- Martelli/embrulho.py
+++ Martelli/embrulho.py
@@ -1,3 +1,23 @@
+"""
+A classe EmbrulhoRestritor demonstra o uso de composição para limitar
+o acesso a atributos de instâncias de outra classe qualquer.
+
+    >>> rex = Cao()
+    >>> rex.latir()
+    Au!
+    >>> rex.morder()
+    Nhac!
+    >>> rex2 = EmbrulhoRestritor(rex, ['morder'])
+    >>> rex2.latir()
+    Au!
+    >>> rex2.morder()
+    Traceback (most recent call last):
+        ...
+    AttributeError: atributo bloqueado 'morder'
+
+"""
+
+
 class EmbrulhoRestritor:
 
     def __init__(self, embrulhado, bloqueios):
@@ -6,7 +26,7 @@ class EmbrulhoRestritor:
 
     def __getattr__(self, nome):
         if nome in self._bloqueios:
-            msg = 'atributo bloqueado: {!r}'.format(nome)
+            msg = 'atributo bloqueado {!r}'.format(nome)
             raise AttributeError(msg)
         else:
             return getattr(self._embrulhado, nome)
