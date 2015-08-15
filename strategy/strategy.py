fcb76942131c1ab7d6e23d33b0e7b02752360104
Luciano Ramalho <luciano@ramalho.org>
2015-08-15 15:13:29 -0300

    estrategia com funções - solução

--- strategy/strategy.py
+++ strategy/strategy.py
@@ -9,19 +9,19 @@
     >>> cart = [LineItem('banana', 4, .5),  # <2>
     ...         LineItem('apple', 10, 1.5),
     ...         LineItem('watermellon', 5, 5.0)]
-    >>> Order(joe, cart, FidelityPromo())  # <3>
+    >>> Order(joe, cart, fidelity_promo)  # <3>
     <Order total: 42.00 due: 42.00>
-    >>> Order(ann, cart, FidelityPromo())  # <4>
+    >>> Order(ann, cart, fidelity_promo)  # <4>
     <Order total: 42.00 due: 39.90>
     >>> banana_cart = [LineItem('banana', 30, .5),  # <5>
     ...                LineItem('apple', 10, 1.5)]
-    >>> Order(joe, banana_cart, BulkItemPromo())  # <6>
+    >>> Order(joe, banana_cart, bulk_item_promo)  # <6>
     <Order total: 30.00 due: 28.50>
     >>> long_order = [LineItem(str(item_code), 1, 1.0) # <7>
     ...               for item_code in range(10)]
-    >>> Order(joe, long_order, LargeOrderPromo())  # <8>
+    >>> Order(joe, long_order, large_order_promo)  # <8>
     <Order total: 10.00 due: 9.30>
-    >>> Order(joe, cart, LargeOrderPromo())
+    >>> Order(joe, cart, large_order_promo)
     <Order total: 42.00 due: 42.00>
 
 # END CLASSIC_STRATEGY_TESTS
@@ -61,7 +61,7 @@ class Order:  # the Context
         if self.promotion is None:
             discount = 0
         else:
-            discount = self.promotion.discount(self)
+            discount = self.promotion(self)
         return self.total() - discount
 
     def __repr__(self):
@@ -69,38 +69,25 @@ class Order:  # the Context
         return fmt.format(self.total(), self.due())
 
 
-class Promotion(ABC):  # the Strategy: an Abstract Base Class
-
-    @abstractmethod
-    def discount(self, order):
-        """Return discount as a positive dollar amount"""
-
-
-class FidelityPromo(Promotion):  # first Concrete Strategy
+def fidelity_promo(order):  # first Concrete Strategy
     """5% discount for customers with 1000 or more fidelity points"""
-
-    def discount(self, order):
-        return order.total() * .05 if order.customer.fidelity >= 1000 else 0
+    return order.total() * .05 if order.customer.fidelity >= 1000 else 0
 
 
-class BulkItemPromo(Promotion):  # second Concrete Strategy
+def bulk_item_promo(order):  # second Concrete Strategy
     """10% discount for each LineItem with 20 or more units"""
-
-    def discount(self, order):
-        discount = 0
-        for item in order.cart:
-            if item.quantity >= 20:
-                discount += item.total() * .1
-        return discount
+    discount = 0
+    for item in order.cart:
+        if item.quantity >= 20:
+            discount += item.total() * .1
+    return discount
 
 
-class LargeOrderPromo(Promotion):  # third Concrete Strategy
+def large_order_promo(order):  # third Concrete Strategy
     """7% discount for orders with 10 or more distinct items"""
-
-    def discount(self, order):
-        distinct_items = {item.product for item in order.cart}
-        if len(distinct_items) >= 10:
-            return order.total() * .07
-        return 0
+    distinct_items = {item.product for item in order.cart}
+    if len(distinct_items) >= 10:
+        return order.total() * .07
+    return 0
 
 # END CLASSIC_STRATEGY
